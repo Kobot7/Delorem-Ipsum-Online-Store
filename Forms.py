8b64c19d4d41d8b13ec920621f5fa9e3f9bfb7a6
Kobot7 <estherkojialing@gmail.com>
2020-01-10 23:16:49 +0800

    Changed sub-categories option for products

--- Forms.py
+++ Forms.py
@@ -1,6 +1,42 @@
 from wtforms import Form, StringField, SelectField, validators, BooleanField, PasswordField, FileField, DecimalField, IntegerField
 
-choicesList = [('', 'Select'), ('Bodycare', 'Bodycare'), ('Lotion', 'Lotion'), ('Over the Counter', 'Over the Counter'), ('Supplements', 'Supplements'), ('Toner', 'Toner'), ('Vitamins', 'Vitamins')]
+choicesList = [('', 'Select')
+                , ('Accessories', 'Baby - Accessories')
+                , ('BabyVitamins', 'Baby - Baby Vitamins')
+                , ('Diapers', 'Baby - Diapers')
+                , ('MilkPowder&Food', 'Baby - Milk Powder & Food')
+                , ('Toiletries', 'Baby - Toiletries')
+
+                , ('Accessories', 'Cosmetics - Accessories')
+                , ('Eyes', 'Cosmetics - Eyes')
+                , ('Face', 'Cosmetics - Face')
+                , ('Lips', 'Cosmetics - Lips')
+                , ('MakeupRemover', 'Cosmetics - Makeup Remover')
+                , ('Nails', 'Cosmetics - Nails')
+
+                , ('Conditioner', 'Hair Care - Conditioner')
+                , ('HairDye', 'Hair Care - Hair Dye')
+                , ('HairStyling', 'Hair Care - Hair Styling')
+                , ('Shampoo', 'Hair Care - Shampoo')
+                , ('Treatment', 'Hair Care - Treatment')
+
+                , ('Eye&EarCare', 'Health - Eye & Ear Care')
+                , ('FamilyPlanning', 'Health - Family Planning')
+                , ('FirstAid&Surgical', 'Health - First Aid & Surgical')
+                , ('Pain&Fever', 'Health - Pain & Fever')
+                , ('Supplements', 'Health - Supplements')
+
+                , ('Accessories', 'Skin Care - Accessories')
+                , ('Anti-acne', 'Skin Care - Anti-acne')
+                , ('Facial', 'Skin Care - Facial')
+                , ('Hand&Body', 'Skin Care - Hand & Body')
+                , ('LipCare', 'Skin Care - Lip Care')
+                , ('SunCare', 'Skin Care - Sun Care')
+
+                , ('Bath&HandCleansing', 'Toiletries - Bath & Hand Cleansing')
+                , ('OralCare', 'Toiletries - Oral Care')
+                , ('SanitaryCare', 'Toiletries - Sanitary Care')
+                , ('Tissues&Wipes', 'Toiletries - Tissues & Wipes')]
 
 class EditProductForm(Form):
     activated = BooleanField('')
